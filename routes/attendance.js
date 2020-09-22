0c14ebfde94a0fbdf7faec2e6603c6a4f2d7da0b
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2020-09-22 19:31:49 +0700

    add authentication in every request

--- routes/attendance.js
+++ routes/attendance.js
@@ -2,9 +2,9 @@ const router = require('express').Router();
 const { AttendanceController } = require('../controllers');
 const { isAdminAuthenticated, isEmployeeAuthenticated } = require('../middlewares/auth');
 
-router.post('/', AttendanceController.addAttendance);
-router.get('/', AttendanceController.getAttendances);
-router.delete('/remove/:id', AttendanceController.deleteAttendance);
-router.delete('/truncate/', AttendanceController.truncateAttendance);
+router.post('/', isEmployeeAuthenticated, AttendanceController.addAttendance);
+router.get('/', isAdminAuthenticated, AttendanceController.getAttendances);
+router.delete('/remove/:id', isAdminAuthenticated, AttendanceController.deleteAttendance);
+router.delete('/truncate/', isAdminAuthenticated, AttendanceController.truncateAttendance);
 
 module.exports = router;
