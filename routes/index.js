617504c52b713594c61787aa9eb7f9f9329fd857
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2020-09-24 10:26:05 +0700

    add request insert data from csv file

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,49 @@ const router = require('express').Router();
 const adminRoutes = require('./admin');
 const employeeRoutes = require('./employee');
 const attendanceRoutes = require('./attendance');
+const fs = require('fs');
+const multer = require('multer');
+const csv = require('csv');
+const upload = multer({ dest: 'tmp/csv/' });
+const { Employee } = require('../models');
 
 const controllerShift = require('../controllers/shift')
 const { isAdminAuthenticated } = require('../middlewares/auth');
+
+
+router.post('/upload-csv', upload.single('file'), function (req, res) {
+  var input = fs.createReadStream(req.file.path);
+  var parser = csv.parse({
+      delimiter: ',',
+      columns: true
+  });
+
+  var transform = csv.transform(function(row) {
+      var resultObj = {
+        nik: row.nik,
+        name: row.name,
+        position: row.position,
+        username: row.username,
+        password: row.password,
+        shiftId: row.shiftId
+      }
+      console.log(resultObj)
+      Employee.create(resultObj)
+          .then(function() {
+            res.status(200).json({
+              message: 'Record created'
+            })
+          })
+          .catch(function(err) {
+            res.status(500).json({
+              message: 'Error encountered: ' + err
+            })
+          })
+  })
+
+  input.pipe(parser).pipe(transform)
+});
+
 
 router.get('/', (req,res) => {
   res.send('Welcome to HRIS - Air Indonesia API 🚀');
