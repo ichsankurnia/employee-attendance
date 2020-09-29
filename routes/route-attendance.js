9df4e4883942a6a00c62423ee36b304deec09b50
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2020-09-29 14:29:18 +0700

    add route attendance today and by date

--- routes/route-attendance.js
+++ routes/route-attendance.js
@@ -6,5 +6,7 @@ router.post('/', isEmployeeAuthenticated, AttendanceController.addAttendance);
 router.get('/', isAdminAuthenticated, AttendanceController.getAttendances);
 router.delete('/remove/:id', isAdminAuthenticated, AttendanceController.deleteAttendance);
 router.delete('/truncate/', isAdminAuthenticated, AttendanceController.truncateAttendance);
+router.get('/sync-today/', isAdminAuthenticated, AttendanceController.getAttendanceToday);
+router.get('/sync-date/:date', isAdminAuthenticated, AttendanceController.getAttendanceByDate);
 
 module.exports = router;
